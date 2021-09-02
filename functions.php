ecf4c7dc2713841dc7a74e1751a135d4f30d7bdc
Abhijeet9850 <67740047+Abhijeet9850@users.noreply.github.com>
2021-09-02 18:00:17 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -22,3 +22,25 @@ endif;
 add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+add_action( 'woocommerce_before_single_product', 'cspl_change_single_product_layout' );
+function cspl_change_single_product_layout() {
+    // Disable the hooks so that their order can be changed.
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+    remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50 );
+
+    // Put the price first.
+    add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 5 );
+    // Include the category/tags info.
+    add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 10 );
+    // Then the product short description.
+    add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 40 );
+    // Move the title to near the end.
+    add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 50 );
+    // And finally include the 'Add to cart' section.
+    add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 60 );
+}
